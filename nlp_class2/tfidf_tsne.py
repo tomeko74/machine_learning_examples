998ece3d69d1be3abf8a2fb7cc3b9be75eb0a52d
tomeko74 <thomasz.orlowski@gmail.com>
2019-07-08 21:08:26 +0200

    add wp2txt files, run, and small changes

--- nlp_class2/tfidf_tsne.py
+++ nlp_class2/tfidf_tsne.py
@@ -7,7 +7,6 @@ from builtins import range
 # Note: you may need to update your version of future
 # sudo pip install -U future
 
-
 import json
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,7 +33,7 @@ def main():
         ('paris', 'france', 'italy'),
     )
 
-    ### choose a data source ###
+    # choose a data source ###
     # sentences, word2idx = get_sentences_with_word2idx_limit_vocab(n_vocab=1500)
     sentences, word2idx = get_wikipedia_data(n_files=3, n_vocab=2000, by_paragraph=True)
     # with open('tfidf_word2idx.json', 'w') as f:
@@ -50,7 +49,6 @@ def main():
     if notfound:
         exit()
 
-
     # build term document matrix
     V = len(word2idx)
     N = len(sentences)
@@ -61,37 +59,41 @@ def main():
     j = 0
     for sentence in sentences:
         for i in sentence:
-            A[i,j] += 1
+            A[i, j] += 1
         j += 1
     print("finished getting raw counts")
 
     transformer = TfidfTransformer()
     A = transformer.fit_transform(A.T).T
+    print("finished Tf-Idf transform")
 
     # tsne requires a dense array
     A = A.toarray()
 
     # map back to word in plot
-    idx2word = {v:k for k, v in iteritems(word2idx)}
+    idx2word = {v: k for k, v in iteritems(word2idx)}
 
     # plot the data in 2-D
     tsne = TSNE()
     Z = tsne.fit_transform(A)
-    plt.scatter(Z[:,0], Z[:,1])
+    print("finished TSNE transform")
+    plt.scatter(Z[:, 0], Z[:, 1])
     for i in range(V):
         try:
             plt.annotate(s=idx2word[i].encode("utf8").decode("utf8"), xy=(Z[i,0], Z[i,1]))
         except:
             print("bad string:", idx2word[i])
+    print("now drawing")
     plt.draw()
 
-    ### multiple ways to create vectors for each word ###
+    # multiple ways to create vectors for each word ###
     # 1) simply set it to the TF-IDF matrix
     # We = A
 
     # 2) create a higher-D word embedding
     tsne = TSNE(n_components=3)
     We = tsne.fit_transform(A)
+    print("finished TSNE transform n_components=3")
 
     # 3) use a classic dimensionality reduction technique
     # svd = KernelPCA(n_components=20, kernel='rbf')
@@ -100,8 +102,9 @@ def main():
     for word_list in analogies_to_try:
         w1, w2, w3 = word_list
         find_analogies(w1, w2, w3, We, word2idx, idx2word)
+    print("finished finding analogies")
 
-    plt.show() # pause script until plot is closed
+    plt.show()  # pause script until plot is closed
 
 
 if __name__ == '__main__':
