665c68f55a8bd8f24f7a3c1ffd3bccfe783e6cb9
tomeko74 <thomasz.orlowski@gmail.com>
2019-07-09 12:57:31 +0200

    fixes

--- nlp_class2/pretrained_glove.py
+++ nlp_class2/pretrained_glove.py
@@ -2,7 +2,7 @@
 # https://www.udemy.com/data-science-natural-language-processing-in-python
 
 # Author: http://lazyprogrammer.me
-from __future__ import print_function, division
+# from __future__ import print_function, division
 from future.utils import iteritems
 from builtins import range
 # Note: you may need to update your version of future
@@ -19,15 +19,18 @@ from sklearn.metrics.pairwise import pairwise_distances
 
 def dist1(a, b):
     return np.linalg.norm(a - b)
+
+
 def dist2(a, b):
     return 1 - a.dot(b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
 
 # pick a distance type
 dist, metric = dist2, 'cosine'
 # dist, metric = dist1, 'euclidean'
 
 
-## more intuitive
+# more intuitive
 # def find_analogies(w1, w2, w3):
 #   for w in (w1, w2, w3):
 #     if w not in word2vec:
@@ -50,41 +53,40 @@ dist, metric = dist2, 'cosine'
 #   print(w1, "-", w2, "=", best_word, "-", w3)
 
 
-## faster
+# faster
 def find_analogies(w1, w2, w3):
-  for w in (w1, w2, w3):
-    if w not in word2vec:
-      print("%s not in dictionary" % w)
-      return
+    for w in (w1, w2, w3):
+        if w not in word2vec:
+            print("%s not in dictionary" % w)
+            return
 
-  king = word2vec[w1]
-  man = word2vec[w2]
-  woman = word2vec[w3]
-  v0 = king - man + woman
+    king = word2vec[w1]
+    man = word2vec[w2]
+    woman = word2vec[w3]
+    v0 = king - man + woman
 
-  distances = pairwise_distances(v0.reshape(1, D), embedding, metric=metric).reshape(V)
-  idxs = distances.argsort()[:4]
-  for idx in idxs:
-    word = idx2word[idx]
-    if word not in (w1, w2, w3): 
-      best_word = word
-      break
+    distances = pairwise_distances(v0.reshape(1, D), embedding, metric=metric).reshape(V)
+    idxs = distances.argsort()[:4]
+    for idx in idxs:
+        word = idx2word[idx]
+        if word not in (w1, w2, w3):
+            best_word = word
+            break
 
-  print(w1, "-", w2, "=", best_word, "-", w3)
+    print(w1, "-", w2, "=", best_word, "-", w3)
 
 
 def nearest_neighbors(w, n=5):
-  if w not in word2vec:
-    print("%s not in dictionary:" % w)
-    return
+    if w not in word2vec:
+        print("%s not in dictionary:" % w)
+        return
 
-  v = word2vec[w]
-  distances = pairwise_distances(v.reshape(1, D), embedding, metric=metric).reshape(V)
-  idxs = distances.argsort()[1:n+1]
-  print("neighbors of: %s" % w)
-  for idx in idxs:
-    print("\t%s" % idx2word[idx])
-
+    v = word2vec[w]
+    distances = pairwise_distances(v.reshape(1, D), embedding, metric=metric).reshape(V)
+    idxs = distances.argsort()[1:n+1]
+    print("neighbors of: %s" % w)
+    for idx in idxs:
+        print("\t%s" % idx2word[idx])
 
 
 # load in pre-trained word vectors
@@ -93,15 +95,15 @@ word2vec = {}
 embedding = []
 idx2word = []
 with open('../large_files/glove.6B/glove.6B.50d.txt', encoding='utf-8') as f:
-  # is just a space-separated text file in the format:
-  # word vec[0] vec[1] vec[2] ...
-  for line in f:
-    values = line.split()
-    word = values[0]
-    vec = np.asarray(values[1:], dtype='float32')
-    word2vec[word] = vec
-    embedding.append(vec)
-    idx2word.append(word)
+    # is just a space-separated text file in the format:
+    # word vec[0] vec[1] vec[2] ...
+    for line in f:
+        values = line.split()
+        word = values[0]
+        vec = np.asarray(values[1:], dtype='float32')
+        word2vec[word] = vec
+        embedding.append(vec)
+        idx2word.append(word)
 print('Found %s word vectors.' % len(word2vec))
 embedding = np.array(embedding)
 V, D = embedding.shape
