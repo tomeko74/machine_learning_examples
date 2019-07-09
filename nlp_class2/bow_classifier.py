fbebeba0a950ce9a21a0ed39e5c7b5de1c9dd2ac
tomeko74 <thomasz.orlowski@gmail.com>
2019-07-09 14:23:00 +0200

    cleanup

--- nlp_class2/bow_classifier.py
+++ nlp_class2/bow_classifier.py
@@ -1,8 +1,8 @@
 # Course URL:
 # https://deeplearningcourses.com/c/natural-language-processing-with-deep-learning-in-python
 # https://udemy.com/natural-language-processing-with-deep-learning-in-python
-from __future__ import print_function, division
-from builtins import range
+# from __future__ import print_function, division
+# from builtins import range
 # Note: you may need to update your version of future
 # sudo pip install -U future
 
@@ -23,108 +23,100 @@ train.columns = ['label', 'content']
 test.columns = ['label', 'content']
 
 
-
 class GloveVectorizer:
-  def __init__(self):
-    # load in pre-trained word vectors
-    print('Loading word vectors...')
-    word2vec = {}
-    embedding = []
-    idx2word = []
-    with open('../large_files/glove.6B/glove.6B.50d.txt') as f:
-      # is just a space-separated text file in the format:
-      # word vec[0] vec[1] vec[2] ...
-      for line in f:
-        values = line.split()
-        word = values[0]
-        vec = np.asarray(values[1:], dtype='float32')
-        word2vec[word] = vec
-        embedding.append(vec)
-        idx2word.append(word)
-    print('Found %s word vectors.' % len(word2vec))
+    def __init__(self):
+        # load in pre-trained word vectors
+        print('Loading word vectors...')
+        word2vec = {}
+        embedding = []
+        idx2word = []
+        with open('../large_files/glove.6B/glove.6B.50d.txt', encoding='utf-8') as f:
+            # is just a space-separated text file in the format:
+            # word vec[0] vec[1] vec[2] ...
+            for line in f:
+                values = line.split()
+                word = values[0]
+                vec = np.asarray(values[1:], dtype='float32')
+                word2vec[word] = vec
+                embedding.append(vec)
+                idx2word.append(word)
+        print('Found %s word vectors.' % len(word2vec))
 
-    # save for later
-    self.word2vec = word2vec
-    self.embedding = np.array(embedding)
-    self.word2idx = {v:k for k,v in enumerate(idx2word)}
-    self.V, self.D = self.embedding.shape
+        # save for later
+        self.word2vec = word2vec
+        self.embedding = np.array(embedding)
+        self.word2idx = {v:k for k,v in enumerate(idx2word)}
+        self.V, self.D = self.embedding.shape
 
-  def fit(self, data):
-    pass
+    def fit(self, data):
+        pass
 
-  def transform(self, data):
-    X = np.zeros((len(data), self.D))
-    n = 0
-    emptycount = 0
-    for sentence in data:
-      tokens = sentence.lower().split()
-      vecs = []
-      for word in tokens:
-        if word in self.word2vec:
-          vec = self.word2vec[word]
-          vecs.append(vec)
-      if len(vecs) > 0:
-        vecs = np.array(vecs)
-        X[n] = vecs.mean(axis=0)
-      else:
-        emptycount += 1
-      n += 1
-    print("Numer of samples with no words found: %s / %s" % (emptycount, len(data)))
-    return X
+    def transform(self, data):
+        X = np.zeros((len(data), self.D))
+        n = 0
+        emptycount = 0
+        for sentence in data:
+            tokens = sentence.lower().split()
+            vecs = []
+            for word in tokens:
+                if word in self.word2vec:
+                    vec = self.word2vec[word]
+                    vecs.append(vec)
+            if len(vecs) > 0:
+                vecs = np.array(vecs)
+                X[n] = vecs.mean(axis=0)
+            else:
+                emptycount += 1
+            n += 1
+        print("Numer of samples with no words found: %s / %s" % (emptycount, len(data)))
+        return X
 
-  def fit_transform(self, data):
-    self.fit(data)
-    return self.transform(data)
-
-
+    def fit_transform(self, data):
+        self.fit(data)
+        return self.transform(data)
 
 
 class Word2VecVectorizer:
-  def __init__(self):
-    print("Loading in word vectors...")
-    self.word_vectors = KeyedVectors.load_word2vec_format(
-      '../large_files/GoogleNews-vectors-negative300.bin',
-      binary=True
-    )
-    print("Finished loading in word vectors")
+    def __init__(self):
+        print("Loading in word vectors...")
+        self.word_vectors = KeyedVectors.load_word2vec_format('../large_files/GoogleNews-vectors-negative300.bin', binary=True)
+        print("Finished loading in word vectors")
 
-  def fit(self, data):
-    pass
+    def fit(self, data):
+        pass
 
-  def transform(self, data):
-    # determine the dimensionality of vectors
-    v = self.word_vectors.get_vector('king')
-    self.D = v.shape[0]
+    def transform(self, data):
+        # determine the dimensionality of vectors
+        v = self.word_vectors.get_vector('king')
+        self.D = v.shape[0]
 
-    X = np.zeros((len(data), self.D))
-    n = 0
-    emptycount = 0
-    for sentence in data:
-      tokens = sentence.split()
-      vecs = []
-      m = 0
-      for word in tokens:
-        try:
-          # throws KeyError if word not found
-          vec = self.word_vectors.get_vector(word)
-          vecs.append(vec)
-          m += 1
-        except KeyError:
-          pass
-      if len(vecs) > 0:
-        vecs = np.array(vecs)
-        X[n] = vecs.mean(axis=0)
-      else:
-        emptycount += 1
-      n += 1
-    print("Numer of samples with no words found: %s / %s" % (emptycount, len(data)))
-    return X
+        X = np.zeros((len(data), self.D))
+        n = 0
+        emptycount = 0
+        for sentence in data:
+            tokens = sentence.split()
+            vecs = []
+            m = 0
+            for word in tokens:
+                try:
+                    # throws KeyError if word not found
+                    vec = self.word_vectors.get_vector(word)
+                    vecs.append(vec)
+                    m += 1
+                except KeyError:
+                    pass
+            if len(vecs) > 0:
+                vecs = np.array(vecs)
+                X[n] = vecs.mean(axis=0)
+            else:
+                emptycount += 1
+            n += 1
+        print("Numer of samples with no words found: %s / %s" % (emptycount, len(data)))
+        return X
 
-
-  def fit_transform(self, data):
-    self.fit(data)
-    return self.transform(data)
-
+    def fit_transform(self, data):
+        self.fit(data)
+        return self.transform(data)
 
 
 vectorizer = GloveVectorizer()
@@ -134,8 +126,6 @@ Ytrain = train.label
 
 Xtest = vectorizer.transform(test.content)
 Ytest = test.label
-
-
 
 # create the model, train it, print scores
 model = RandomForestClassifier(n_estimators=200)
